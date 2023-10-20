a0329601eb8c3599c82e51637d42f8317fa82ad2
kevinfmunoz <km626728283@gmail.com>
2023-10-20 19:03:21 -0300

    terminado

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-
+from controller.usuariobuilder import UsuarioBuilder
 from controller.atmcontroller import ATMController
 from domain.atmmodel import ATMModel
 
@@ -8,3 +8,25 @@ if __name__ == "__main__":
 
     amount = int(input("Enter the amount to withdraw: $"))
     controller.run(amount)
+
+def main():
+    usuario_builder = UsuarioBuilder()
+    
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    edad = int(input("Edad: "))
+    email = input("Email: ")
+    direccion = input("Dirección: ")
+    
+    usuario = usuario_builder \
+        .with_nombre(nombre) \
+        .with_apellido(apellido) \
+        .with_edad(edad) \
+        .with_email(email) \
+        .with_direccion(direccion) \
+        .build()
+    
+    print(usuario)
+
+if __name__ == "__main__":
+    main()
