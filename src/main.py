7d3bc762cac832b63347a3248141f7ad0e0c0565
kevin <km626728283@gmail.com>
2024-06-18 19:05:56 -0300

    commit

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 import os
 from flask import Flask, send_file
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
-    return send_file('index.html')
-
+    try:
+        return send_file('index.html')
+    except FileNotFoundError:
+        return "Error: El archivo 'index.html' no se encontró.", 404
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
-
 
 if __name__ == "__main__":
     main()
