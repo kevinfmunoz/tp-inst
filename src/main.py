af4f255a0ecf85cd5ebe2b4642d705b328b94f4c
kevin <km626728283@gmail.com>
2024-06-18 19:07:28 -0300

    commit

--- src/main.py
+++ src/main.py
@@ -10,8 +10,11 @@ def index():
     except FileNotFoundError:
         return "Error: El archivo 'index.html' no se encontró.", 404
 
+
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
 
+
 if __name__ == "__main__":
     main()
+
